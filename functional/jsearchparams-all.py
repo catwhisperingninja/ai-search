4074ab19d4ad885eb9313cb831ce6e0f5c000fa5
catwhisperingninja <25212708+catwhisperingninja@users.noreply.github.com>
2024-06-03 01:11:49 -0700

    fixed jsearch script

--- functional/jsearchparams-all.py
+++ functional/jsearchparams-all.py
@@ -1,11 +1,15 @@
+import os
+from dotenv import load_dotenv
 import requests
+
+load_dotenv() 
 
 url = "https://jsearch.p.rapidapi.com/search"
 
 querystring = {"query":"information security analyst in San Francisco USA","page":"1","num_pages":"1","date_posted":"week","employment_types":"FULLTIME","actively_hiring":"true","radius":"15"}
 
 headers = {
-	"x-rapidapi-key": "398a4b4962mshd8284c9a0ccbcb9p1f24fcjsn457f5a0c7d3a",
+	"x-rapidapi-key": os.getenv('X_RapidAPI_Key'), 
 	"x-rapidapi-host": "jsearch.p.rapidapi.com"
 }
 
